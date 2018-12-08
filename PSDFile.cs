dc739dcbcefb64fa7405a21e5215f2acb81d8b13
Jason Nelson <jason@carbonmade.com>
2018-12-07 20:49:58 -0800

    Breakout ImageCompression

--- PSDFile.cs
+++ PSDFile.cs
@@ -416,34 +416,5 @@ namespace System.Drawing.PSD
 
             _globalLayerMaskData = reader.ReadBytes((Int32)maskLength);
         }
-
-    }
-
-    public enum ImageCompression
-    {
-        /// <summary>
-        /// Raw data
-        /// </summary>
-        Raw = 0,
-        /// <summary>
-        /// RLE compressed
-        /// </summary>
-        Rle = 1,
-        /// <summary>
-        /// ZIP without prediction.
-        /// <remarks>
-        /// This is currently not supported since it is ot documented.
-        /// Loading will result in an image where all channels are set to zero.
-        /// </remarks>
-        /// </summary>
-        Zip = 2,
-        /// <summary>
-        /// ZIP with prediction.
-        /// <remarks>
-        /// This is currently not supported since it is ot documented. 
-        /// Loading will result in an image where all channels are set to zero.
-        /// </remarks>
-        /// </summary>
-        ZipPrediction = 3
     }
 }
